a836dcd8f1d0a5357e0fcd154788b43282cfe33a
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2024-09-02 07:36:04 +0600

    Create Feature scaling for Machine Learning.py

---
+++ Data Preprocessing/Feature scaling for Machine Learning.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Load the Wine Quality Red dataset
+df = pd.read_csv('winequality-red.csv', delimiter=';')
+
+# Separate features and target
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+# Split the dataset into an 80-20 training-test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create an instance of the StandardScaler class
+sc = StandardScaler()
+
+# Fit the StandardScaler on the features from the training set and transform it
+X_train = sc.fit_transform(X_train)
+
+# Apply the transform to the test set
+X_test = sc.transform(X_test)
+# Apply the transform to the test set
+print(X_test)
+print(X_train)
+print(y_test)
+print(y_train)
